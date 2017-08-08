ff764816190f1e9957469be12d59b2ca70aaa6aa
Alexis Viguié <net@siphonay.fr>
2017-08-08 13:23:10 +0200

    integrate def into bot routine

--- antoinefromafar_bot.rb
+++ antoinefromafar_bot.rb
@@ -6,29 +6,6 @@ require 'http-cookie'
 
 abort "usage: #{$PROGRAM_NAME} telegram_token cookie_file" unless ARGV.length == 2
 
-def get_acapela_tts str
-  acapela_uri = URI.parse("http://www.acapela-group.com/demo-tts/DemoHTML5Form_V2.php")
-  acapela_cookies = HTTP::CookieJar.new
-  acapela_cookies.load(ARGV[1], :cookiestxt) if File.exist?(ARGV[1])
-  
-  acapela_request = Net::HTTP::Post.new acapela_uri
-  acapela_request.form_data =
-    {
-      "MyLanguages" => "sonid10",
-      "MySelectedVoice" => "AntoineFromAfar",
-      "MyTextForTTS" => str,
-      "t" => "1",
-      "SendToVaaS" => "",
-    }
-  acapela_request['Cookie'] = HTTP::Cookie.cookie_value(acapela_cookies.cookies(acapela_uri))
-  
-  acapela_response = Net::HTTP.start(acapela_uri.hostname, acapela_uri.port) do |http|
-    http.request acapela_request
-  end
-  puts acapela_response.body.scan(/http.*?mp3/).join
-  return acapela_response.body.scan(/http.*?mp3/).join
-end
-
 begin
   Telegram::Bot::Client.run(ARGV[0]) do |antoine_bot|
     antoine_bot.listen do |message|
@@ -37,10 +14,33 @@ begin
         antoine_bot.api.send_message(chat_id: message.chat.id,
                              text: "Ce bot s'utilise en mode \"inline\", mentionnez le dans une conversation pour l'utiliser.")
       when Telegram::Bot::Types::InlineQuery
+        if message.query.size == 0
+          acapela_inline_query = " "
+        else
+          acapela_inline_query = message.query
+        acapela_uri = URI.parse("http://www.acapela-group.com/demo-tts/DemoHTML5Form_V2.php")
+        acapela_cookies = HTTP::CookieJar.new
+        acapela_cookies.load(ARGV[1], :cookiestxt) if File.exist?(ARGV[1])
+        
+        acapela_request = Net::HTTP::Post.new acapela_uri
+        acapela_request.form_data =
+          {
+            "MyLanguages" => "sonid10",
+            "MySelectedVoice" => "AntoineFromAfar",
+            "MyTextForTTS" => acapela_inline_query,
+            "t" => "1",
+            "SendToVaaS" => "",
+          }
+        acapela_request['Cookie'] = HTTP::Cookie.cookie_value(acapela_cookies.cookies(acapela_uri))
+        
+        acapela_response = Net::HTTP.start(acapela_uri.hostname, acapela_uri.port) do |http|
+          http.request acapela_request
+        end
+        acapela_voice_url = acapela_response.body.scan(/http.*?mp3/).join
         result = [
           ["voice",
            "1",
-           get_acapela_tts(message.query),
+           acapela_voice_url,
           "AntoineFromAfar dit:"]
         ].map do |arr|
           Telegram::Bot::Types::InlineQueryResultVoice.new(
